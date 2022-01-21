c8e7044091999fe8898a8bc82a0806571a7f27d1
kseki <kota.yt@gmail.com>
2022-01-21 23:11:13 +0900

    Exec rubocop auto correct

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,16 +6,16 @@ class Note < ApplicationRecord
 
   validates :message, presence: true
 
-  scope :search, ->(term) {
-    where("LOWER(message) LIKE ?", "%#{term.downcase}%")
+  scope :search, lambda { |term|
+    where('LOWER(message) LIKE ?', "%#{term.downcase}%")
   }
 
   has_one_attached :attachment
 
   validates :attachment, blob: { content_type: [
-    "image/jpeg",
-    "image/gif",
-    "image/png",
-    "application/pdf",
-  ]}
+    'image/jpeg',
+    'image/gif',
+    'image/png',
+    'application/pdf'
+  ] }
 end
