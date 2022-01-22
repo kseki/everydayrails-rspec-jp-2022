53641da8aa8889f5d159ad40de5d3bcf4ce9c01c
kseki <kota.yt@gmail.com>
2022-01-22 20:49:00 +0900

    describe, context, before, afterを使ってスペックをDRYにする

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,62 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  # 検索文字列に一致するメモを返すこと
-  it 'returns notes that match the search term' do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: 'Joe',
       last_name: 'Tester',
       email: 'tester@example.com',
       password: 'dottle-nouveau-pavilion-tight-furze'
     )
 
-    project = user.projects.create(
+    @project = @user.projects.create(
       name: 'Test Project'
     )
-
-    note1 = project.notes.create(
-      message: 'This is the first note.',
-      user:
-    )
-    note2 = project.notes.create(
-      message: 'This is the second note.',
-      user:
-    )
-    note3 = project.notes.create(
-      message: 'First, preheat the oven.',
-      user:
-    )
-
-    expect(described_class.search('first')).to include(note1, note3)
-    expect(described_class.search('first')).not_to include(note2)
   end
 
-  # 検索結果が1件も見つからなければ空のコレクションを返すこと
-  it 'returns an empty collection when no results are found' do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'dottle-nouveau-pavilion-tight-furze'
+  # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
+  it 'is valid with a user, project, and message' do
+    note = described_class.new(
+      message: 'This is a sample note.',
+      user: @user,
+      project: @project
     )
 
-    project = user.projects.create(
-      name: 'Test Project'
-    )
+    expect(note).to be_valid
+  end
 
-    note1 = project.notes.create(
-      message: 'This is the first note.',
-      user:
-    )
-    note2 = project.notes.create(
-      message: 'This is the second note.',
-      user:
-    )
-    note3 = project.notes.create(
-      message: 'First, preheat the oven.',
-      user:
-    )
+  # メッセージがなければ無効な状態であること
+  it 'is invalid without a message' do
+    note = described_class.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include("can't be blank")
+  end
 
-    expect(described_class.search('message')).to be_empty
+  # 文字列に一致するメッセージを検索する
+  describe 'search message for a term' do
+    before do
+      @note1 = @project.notes.create(
+        message: 'This is the first note.',
+        user: @user
+      )
+      @note2 = @project.notes.create(
+        message: 'This is the second note.',
+        user: @user
+      )
+      @note3 = @project.notes.create(
+        message: 'First, preheat the oven.',
+        user: @user
+      )
+    end
+
+    # 一致するデータが見つかる時
+    context 'when a match is found' do
+      # 検索文字列に一致するメモを返すこと
+      it 'returns notes that match the search term' do
+        expect(described_class.search('first')).to include(@note1, @note3)
+      end
+    end
+
+    # 一致するデータが見つからない時
+    context 'when no match is found' do
+      # 検索結果が1件も見つからなければ空のコレクションを返すこと
+      it 'returns an empty collection when no results are found' do
+        expect(described_class.search('message')).to be_empty
+      end
+    end
   end
 end
