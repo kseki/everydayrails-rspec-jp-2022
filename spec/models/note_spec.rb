08155570a042cc74b3d30a581128d282748fd253
kseki <kota.yt@gmail.com>
2022-01-22 20:20:34 +0900

    失敗をテストする

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -30,4 +30,33 @@ RSpec.describe Note, type: :model do
     expect(described_class.search('first')).to include(note1, note3)
     expect(described_class.search('first')).not_to include(note2)
   end
+
+  # 検索結果が1件も見つからなければ空のコレクションを返すこと
+  it 'returns an empty collection when no results are found' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tight-furze'
+    )
+
+    project = user.projects.create(
+      name: 'Test Project'
+    )
+
+    note1 = project.notes.create(
+      message: 'This is the first note.',
+      user:
+    )
+    note2 = project.notes.create(
+      message: 'This is the second note.',
+      user:
+    )
+    note3 = project.notes.create(
+      message: 'First, preheat the oven.',
+      user:
+    )
+
+    expect(described_class.search('message')).to be_empty
+  end
 end
