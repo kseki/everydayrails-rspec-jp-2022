fba018a40314295b2d46d6ac91157a43bdc984ee
kseki <kota.yt@gmail.com>
2022-01-25 23:26:30 +0900

    JavaScriptを使った操作をテストする

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
   scenario 'user creates a new project' do
     user = FactoryBot.create(:user)
 
@@ -20,11 +16,5 @@ RSpec.describe "Projects", type: :system do
       fill_in 'Description', with: 'Trying out Capybara'
       click_button 'Create Project'
     }.to change(user.projects, :count).by(1)
-  end
-
-  scenario 'guest adds a project' do
-    visit projects_path
-    save_and_open_page
-    click_link 'New Project'
   end
 end
