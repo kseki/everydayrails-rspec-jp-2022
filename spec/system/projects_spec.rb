6321f317c7afbe2cfb7df785cad204ba4283afc2
kseki <kota.yt@gmail.com>
2022-01-29 21:41:55 +0900

    aggregate_failures （失敗の集約）

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe "Projects", type: :system do
       fill_in 'Description', with: 'Trying out Capybara'
       click_button 'Create Project'
     }.to change(user.projects, :count).by(1)
+
+    aggregate_failures do
+      expect(page).to have_content 'Project was successfully created'
+      expect(page).to have_content 'Test Project'
+      expect(page).to have_content "Owner: #{user.name}"
+    end
   end
 end
